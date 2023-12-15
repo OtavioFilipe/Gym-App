1d21993c2f049409ba0b78d26f550a14b3c7f0f0
OtavioFilipe <darksnack16@gmail.com>
2023-12-15 19:22:03 -0300

    add routes

--- App.tsx
+++ App.tsx
@@ -6,9 +6,10 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
+import { Routes } from "./src/routes";
+
 import { THEME } from "./src/theme";
 import { Loading } from "@components/Loading";
-import { SignUp } from "@screens/SignUp";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -21,7 +22,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
